ddcb959ca4efb52ee54be1b3f63fb33b0233c829
AntCannon <anthonycannonier@gmail.com>
2024-02-09 14:20:41 -0500

    move user data fetch to WelcomeSection component, works.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-// App.js
+// imports 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import WelcomeSection from './components/WelcomeSection';
 import ChallengeCard from './components/ChallengeCard';
 import ApiData from './components/ApiData';
+import FCCTutorial from './FCCTutorial';
 
+// App.js
 const App = () => {
-  const [apiResponse, setApiResponse] = useState([]);
-  
-  useEffect(() => {
-    // Update the API URL
-    const apiUrl = 'http://www.codewars.com/api/v1/users/AntCannon/code-challenges/completed?page=0';
-
-    // Make API request to Codewars
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(apiUrl);
-        setApiResponse(response.data);
-      } catch (error) {
-        console.error('Error fetching Codewars data:', error);
-      }
-    };
-
-    fetchData();
-  }, []); // Empty dependency array ensures useEffect runs only once
 
   return (
     <div>
       <WelcomeSection />
-      <p>{ apiResponse.id }</p>
-      <ApiData />
+      {/* <p>{ apiResponse.id }</p> */}
+      {/* <ApiData /> */}
       <div>
-        <ChallengeCard />
+        {/* <ChallengeCard /> */}
       </div>
     </div>
   );
 };
 
+// export
 export default App;
--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -1,13 +1,40 @@
-// Header.js
+// imports
 import React from "react";
+import axios from "axios";
 
+// api variables
+const userName = 'AntCannon';
+const getUserURL = `https://www.codewars.com/api/v1/users/${userName}`
+
+
+// WelcomeSection
 const WelcomeSection = () => {
+  const [userData, setUserData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    // Update the API URL
+    const apiUrl = 'http://www.codewars.com/api/v1/users/AntCannon/code-challenges/completed?page=0';
+
+    // Make API request to Codewars
+    axios.get(getUserURL)
+      .then(response => setUserData(response.data))
+      .catch(error => setError(error))
+
+  }, []); // Empty dependency array ensures useEffect runs only once
+
+  if (error) return `Error: ${error.message}`
+  if (!userData) return null;
 
   return(
     <>
     <h1>This is the Welcome Section</h1>
+    <h1>{userData.username}</h1>
+    <h2>Rank: {userData.ranks.overall.name} | Honor: {userData.honor}</h2>
     </>
   )
 };
 
+
+// export
 export default WelcomeSection;
