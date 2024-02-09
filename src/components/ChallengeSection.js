ecb77b8975dd6a5d725aff5871d67b8f1b153c2f
AntCannon <anthonycannonier@gmail.com>
2024-02-09 15:34:00 -0500

    add ChallengeCard component with name and date props

--- src/components/ChallengeSection.js
+++ src/components/ChallengeSection.js
@@ -8,15 +8,14 @@ const page = 0;
 const getCompletedChallengesURL = `https://www.codewars.com/api/v1/users/AntCannon/code-challenges/completed?page=0}`
 
 // Challenge Card
-// const ChallengeCard = (props) => {
-//   return(
-//     <>
-//     <h2></h2>
-//     </>
-//   )
-// }
-
-
+const ChallengeCard = (props) => {
+  return(
+    <>
+    <h2>Kata: {props.name}</h2>
+    <h3>Date: {props.date}</h3>
+    </>
+  )
+}
 
 // ChallengeSection.js
 const ChallengeSection = () => {
@@ -37,9 +36,16 @@ const ChallengeSection = () => {
   return (
     <div>
       <h1>Daily Challenges should be Here</h1>
-      <h2>{challengeData[0].name}</h2>
+      <ChallengeCard name={challengeData[0].name} date={dispDate(challengeData[0])}/>
+      <h3></h3>
+      {/* <h2>{challengeData[0].name}</h2> */}
     </div>
   );
 };
+
+// Helper function - display date and time
+function dispDate(challenge) {
+  return new Date(challenge.completedAt).toString().slice(0,-33)
+}
 
 export default ChallengeSection;
