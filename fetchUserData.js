39f4554ec5a9b4165066cdfdd870cb4bc28e7da9
AntCannon <anthonycannonier@gmail.com>
2024-02-09 14:19:56 -0500

    get fetchUserData.js to fetch my codewatr user data using asyn await.

---
+++ fetchUserData.js
@@ -0,0 +1,31 @@
+const userName = 'AntCannon';
+const getUserURL = `https://www.codewars.com/api/v1/users/${userName}`
+
+let userInfo;
+
+async function fetchUserData() {
+  const userData = await fetch(getUserURL)
+    .then(response => response.json())
+    .then(json => userInfo = json);
+  return userInfo
+}
+
+fetchUserData().then(userData => console.log(userData))
+console.log(`variable`,userInfo)
+
+// function resolveAfter2Seconds() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve('resolved');
+//     }, 2000);
+//   });
+// }
+
+// async function asyncCall() {
+//   console.log('calling');
+//   const result = await resolveAfter2Seconds();
+//   console.log(result);
+//   // Expected output: "resolved"
+// }
+
+// asyncCall();
